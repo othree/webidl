e35e76b7f55931b2bcc6daa06d8a8bacd070a66f
othree <othree@gmail.com>
2017-02-14 11:52:49 +0800

    Use dxr

--- index.js
+++ index.js
@@ -3,24 +3,44 @@ var cheerio = require('cheerio');
 
 var exec = require('sync-exec');
 
-var base = 'https://mxr.mozilla.org';
+var base = 'https://dxr.mozilla.org';
 
-request(base + '/mozilla-central/source/dom/webidl/', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+var sourcePath = '/mozilla-central/source';
+var rawPath = '/mozilla-central/raw-file';
+var path = '/dom/webidl';
+
+request(base + sourcePath + path + '/', function (error, response, body) {
+  if (!error && response.statusCode === 200) {
     var $ = cheerio.load(body);
 
-    var links = $('td[align=left] > a[href*=webidl]');
+    var links = $('a.conf');
 
-    links.map(function (i, node) {
-      var pathname = node.attribs.href
-      var url = base + pathname + '?raw=1';
-      var frags = pathname.split('/');
-      var filename = frags[frags.length - 1];
-      if (/\.webidl$/.test(filename)) {
-        console.log('wget -q "' + url + '" -O ' + filename);
-        exec('wget -q "' + url + '" -O webidl/' + filename);
+    var pathname = links[0].attribs.href;
+    var url = base + pathname;
+
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var $ = cheerio.load(body);
+
+        var link = $('a.raw');
+        if (link.length) {
+          var pathname = link[0].attribs.href;
+          var frags = pathname.split('/');
+
+          var rawBase = base + rawPath + '/' + frags[5] + path;
+
+          links.map(function (i, node) {
+            var pathname = node.attribs.href;
+            var frags = pathname.split('/');
+            var filename = frags[frags.length - 1];
+            var url = rawBase + '/' + filename;
+            if (/\.webidl$/.test(filename)) {
+              console.log('wget -q "' + url + '" -O ' + filename);
+              exec('wget -q "' + url + '" -O webidl/' + filename);
+            }
+          });
+        }
       }
     });
-
   }
 });
